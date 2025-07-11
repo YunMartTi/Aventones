f2fa343716bc09450ef9640939d7dd14f3b072e2
Yunior Martinez <yunmartsoftware@gmail.com>
2025-07-10 20:53:28 -0600

    Primer proyecto Aventones

--- resources/views/Rides/AddRide.blade.php
+++ resources/views/Rides/AddRide.blade.php
@@ -57,7 +57,7 @@
                     </datalist>
                 </div>
                 <div class="form-group">
-                    <label>Models</label>
+                    <label>Modelss</label>
                     <input type="text" name="model" id="model" required>
                 </div>
                 <div class="form-group">
